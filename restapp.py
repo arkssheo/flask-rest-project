f490ae2e63097697ae377fdc1e0baee96a333aab
Jose Martinez <arkharl@LunaMBA.local>
2018-01-02 23:39:20 -0600

    put

--- restapp.py
+++ restapp.py
@@ -30,6 +30,21 @@ class Item(Resource):
     items.append(item)
     return item, 201
 
+  def delete(self, name):
+    global items
+    items = list(filter(lambda x: x['name'] != name, items))
+    return {'message': 'Item deleted'}
+
+  def put(self, name):
+    data = request.get_json()
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    if item is None:
+      item = {'name': name, 'price': data['price']}
+      items.append(item)
+    else:
+      item.update(data) # python native o_o!
+    return item
+
 class ItemList(Resource):
   def get(self):
     return {'items': items}, 201
