37b30b3d15d4a24001039cad7371dac3b771b565
Jose Martinez <arkharl@LunaMBA.local>
2018-01-11 22:58:50 -0600

    added sqlite db manually

--- user.py
+++ user.py
@@ -1,5 +1,42 @@
+import sqlite3
+
 class User:
-  def __init__(self, _id, username, password):
+  def __init__(self, _id, username, password, email):
     self.id = _id
     self.username = username
     self.password = password
+    self.email = email
+  
+  @classmethod
+  def find_by_username(cls, username):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM users WHERE username = ?"
+    result = cursor.execute(query, (username,)) # always needs to be tuple (needs trailing comma)
+
+    row = result.fetchone()
+    if row:
+      user = cls(*row)
+    else:
+      user = None
+    
+    connection.close()
+    return user
+
+  @classmethod
+  def find_by_id(cls, _id):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM users WHERE id = ?"
+    result = cursor.execute(query, (_id,)) # always needs to be tuple (needs trailing comma)
+
+    row = result.fetchone()
+    if row:
+      user = cls(*row)
+    else:
+      user = None
+    
+    connection.close()
+    return user
