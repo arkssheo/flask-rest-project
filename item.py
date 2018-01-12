eeeaa85f7201d455da77f6df764ad0c3afa6eb17
Jose Martinez <arkharl@LunaMBA.local>
2018-01-11 23:57:02 -0600

    manual db finish

---
+++ item.py
@@ -0,0 +1,121 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+class Item(Resource):
+  parser = reqparse.RequestParser()
+  parser.add_argument('price', 
+    type = float,
+    required = True,
+    help = 'This field is required!!!!!!!!!'
+  )
+
+  @jwt_required()
+  def get(self, name):
+    item = self.find_by_name(name)
+    if item:
+      return item, 200
+    return {'message': 'item not found'}, 404
+
+  @classmethod
+  def find_by_name(cls, name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM items WHERE name = ?"
+    result = connection.execute(query, (name,))
+    row = result.fetchone()
+
+    connection.close()
+
+    if row:
+      return {'item': {'name': row[0], 'price': row[1]}}
+  
+  def post(self, name):
+    if self.find_by_name(name):
+      return {'message': 'An item with name {} already exists'.format(name)}, 400
+
+    data = Item.parser.parse_args()
+    
+    item = {
+      'name': name, 
+      'price': data['price']
+    }
+
+    try:
+      self.insert(item)
+    except:
+      return {'message': 'server error'}, 500
+
+    return item, 201
+
+  @classmethod
+  def insert(self, item):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "INSERT INTO items VALUES (?, ?)"
+    connection.execute(query, (item['name'], item['price']))
+
+    connection.commit()
+    connection.close()
+
+  def delete(self, name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "DELETE FROM items WHERE name = ?"
+    connection.execute(query, (name,))
+
+    connection.commit()
+    connection.close()
+
+    return {'message': 'Item deleted'}
+
+  def put(self, name):
+    data = Item.parser.parse_args()
+
+    item = self.find_by_name(name)
+    updated_item = {'name': name, 'price': data['price']}
+
+    if item is None:
+      try:
+        self.insert(updated_item)
+      except:
+        return {'message': 'server error'}, 500
+    else:
+      try:
+        self.update(updated_item)
+      except:
+        return {'message': 'server error'}, 500
+    return updated_item
+
+  @classmethod
+  def update(self, item):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "UPDATE items SET price = ? WHERE name = ?"
+    connection.execute(query, (item['price'], item['name']))
+
+    connection.commit()
+    connection.close()
+
+class ItemList(Resource):
+  def get(self):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM items"
+    result = connection.execute(query)
+
+    items = []
+
+    for row in result:
+      items.append({'name': row[0], 'price': row[1]})
+
+    connection.close()
+    
+    return {'items': items}, 201
+
+
